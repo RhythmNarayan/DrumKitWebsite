6a83cb662978af07c4ab6886501c79d201ac111c
RhythmNarayan <115897657+RhythmNarayan@users.noreply.github.com>
2023-12-12 01:28:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        
+        var buttonInnerHTML = this.innerHTML;
+        
+        makeSound(buttonInnerHTML);
+        
+    })  
+    
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var sound = new Audio("sounds/tom-1.mp3")
+            sound.play();
+            break;
+        case "a":
+            var sound = new Audio("sounds/tom-2.mp3")
+            sound.play();
+            break;
+        case "s":
+            var sound = new Audio("sounds/tom-3.mp3")
+            sound.play();
+            break;
+        case "d":
+            var sound = new Audio("sounds/tom-4.mp3")
+            sound.play();
+            break;
+        case "j":
+            var sound = new Audio("sounds/snare.mp3")
+            sound.play();
+            break;
+        case "k":
+            var sound = new Audio("sounds/crash.mp3")
+            sound.play();
+            break;
+        case "l":
+            var sound = new Audio("sounds/Bass-drum-1.mp3")
+            sound.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML);
+    }
+
+}
